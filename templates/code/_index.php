55ce14b5e8d2db3caa400a52500aa0034e65084f
Anas Mazouni <madadj4@gmail.com>
2018-05-24 17:56:06 +0000

    Merged with php-mvc-boilerplate

---
+++ templates/code/_index.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * <%= project.name %><% if (project.homepage) { -%> (<%= project.homepage %>)<% } -%>.
+<% if (project.description) { -%>
+ * <%= project.description %>.
+<% } -%>
+ *
+<% if (control.license) { -%>
+ * @license <%= project.license %>
+<% }
+if (project.homepage) { -%>
+ * @link <%= project.homepage %>
+<% } -%>
+ * @author <%= owner.name %><% if (owner.email) { -%> <<%= owner.email %>><% } -%>
+ */
+
+declare(strict_types=1);
+
+/**
+ * Composer
+ */
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+/**
+ * Enviroment setup
+ */
+
+use Dotenv\Dotenv;
+// Import .env variables and add them the enviroment
+$dotenv = new Dotenv(__DIR__."/../src/");
+$dotenv->load();
+
+/**
+ * Logging
+ */
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+$stream = new StreamHandler('logs/main.log', Logger::DEBUG);
+// Create a logger for the debugging-related stuff
+$logger = new Logger('debug');
+$logger->pushHandler($stream);
+
+/**
+ * Error and Exception handling
+ */
+
+// Whoops error handling
+$whoops = new Whoops\Run();
+// Set Whoops as the default error and exception handler used by PHP:
+$whoops->register();
+$whoops->pushHandler(new Whoops\Handler\PrettyPageHandler());
+
+require_once('../App/Routes.php');
